703f829a82f7aae62d73ed7a66c160e42e854395
ik <wndlsrnr1@gmail.com>
2024-07-05 19:15:10 +0900

    ItemEdit.jsx 수정하기
    image 좌우 이동 추가 삭제
    파일 추가 후 섞여 보이게 함.
    
    request 보냄.

--- src/items/customer/ItemBasicInfo.jsx
+++ src/items/customer/ItemBasicInfo.jsx
@@ -1,36 +1,46 @@
-const ItemBasicInfo = () => {
+import {getDiscountedPrice} from "../../utils/priceUtils";
+import {Col, Row} from "reactstrap";
+
+const ItemBasicInfo = (props) => {
+
+  const {name, nameKor, price, state, description, relatedAt, saleRate, brand, manufacturer, madeIn} = props;
+
   return (
     <div className={"content"}>
-      <div><span>기본정보</span></div>
+      <div><span style={{fontWeight:"bold"}}>기본정보</span></div>
+      <hr/>
       <div>
-        <div>
-          <div><span>정가</span></div>
-          <div><span>10,000</span></div>
-        </div>
-        <div>
-          <div><span>판매가</span></div>
-          <div><span>10,000</span></div>
-        </div>
-        <div>
-          <div><span>브랜드</span></div>
-          <div><span>플스</span></div>
-        </div>
-        <div>
-          <div><span>제조사</span></div>
-          <div><span>소니</span></div>
-        </div>
-        <div>
-          <div><span>원산지</span></div>
-          <div><span>중국</span></div>
-        </div>
-        <div>
-          <div><span>출시일</span></div>
-          <div><span>2023-12-20</span></div>
-        </div>
-        <div>
-          <div><span>베송비</span></div>
-          <div><span>무료</span></div>
-        </div>
+        <Row>
+          <Col><span style={{fontWeight:"bold"}} >정가</span></Col>
+          <Col><span>{price}</span></Col>
+        </Row>
+        <hr/>
+        <Row>
+          <Col><span style={{fontWeight:"bold"}} >판매가</span></Col>
+          <Col><span>{getDiscountedPrice(price, saleRate)}</span></Col>
+        </Row>
+        <hr/>
+        <Row>
+          <Col><span style={{fontWeight:"bold"}} >브랜드</span></Col>
+          <Col><span>{brand}</span></Col>
+        </Row>
+        <hr/>
+        <Row>
+          <Col><span style={{fontWeight:"bold"}} >제조사</span></Col>
+          <Col><span>{manufacturer}</span></Col>
+        </Row>
+        <hr/>
+        <Row>
+          <Col><span style={{fontWeight:"bold"}} >원산지</span></Col>
+          <Col><span>{madeIn}</span></Col>
+        </Row>
+        <hr/>
+        <Row>
+          <Col><span style={{fontWeight:"bold"}} >출시일</span></Col>
+          <Col><span>{relatedAt}</span></Col>
+        </Row>
+        <hr/>
+
       </div>
     </div>
   )
