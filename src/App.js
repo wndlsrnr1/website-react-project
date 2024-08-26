041936221acaa5a3a30b939c1146a3bd1d07052c
ik <wndlsrnr1@gmail.com>
2024-08-26 17:20:55 +0900

    1. endpoint 별 정리
    2. /admin 경로 client 보안

--- src/App.js
+++ src/App.js
@@ -7,57 +7,64 @@ import {createContext, useContext, useEffect, useReducer, useState} from "react"
 import data from "bootstrap/js/src/dom/data";
 import Logout from "./login/Logout";
 import MyPage from "./user/MyPage";
-import AdminMain from "./admin/AdminMain";
-import CategoryManage from "./admin/category/CategoryManage";
-import SubcategoryManage from "./admin/category/SubcategoryManage";
-import ItemManage from "./admin/item/ItemManage";
-import ItemDetail from "./admin/item/ItemDetail";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import ItemAdd from "./admin/item/ItemAdd";
-import ItemEdit from "./admin/item/ItemEdit";
-import HomeItemCarousel from "./admin/item/home/carousel/HomeItemCarousel"
-import HomeItemCarouselAdd from "./admin/item/home/carousel/HomeItemCarouselAdd";
-import HomeItemCarouselDetail from "./admin/item/home/carousel/HomeItemCarouselDetail";
+import {fetchWithAuth} from "./utils/fetchUtils";
+import AdminRoutes from "./routes/admin/AdminRoutes";
+import ItemRoutes from "./routes/item/ItemRoutes";
 import ItemList from "./items/itemList";
 import ItemCustomerDetail from "./items/customer/ItemCustomerDetail";
-import OnDiscountItemList from "./items/customer/OnDiscountItemList";
 import Items from "./items/customer/Items";
-import {fetchWithAuth} from "./utils/fetchUtils";
+import OnDiscountItemList from "./items/customer/OnDiscountItemList";
+import {checkLogin} from "./utils/LoginUtils";
 
 const App = () => {
   const [loaded, setLoaded] = useState(false);
+  //variables
+  const [isAdmin, setIsAdmin] = useState(false);
+  const isLogin = checkLogin(localStorage);
+  //hooks
+
+  //useEffects
+  useEffect(() => {
+    if (loaded) {
+      return;
+    }
+    setLoaded(true);
+
+    if (!isLogin) {
+      return;
+    }
+    const requestAdmin = async () => {
+      fetchWithAuth("/users/admin", {
+        method: "get"
+      })
+        .then(resp => {
+          if (resp.status === 200) {
+            setIsAdmin(true);
+          } else {
+            setIsAdmin(false);
+          }
+        });
+    };
+    requestAdmin();
+  }, [isAdmin, loaded]);
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path={"/"} exact component={HomeMain}/>
-        <Route path={"/login"} exact component={Login}/>
-        <Route path={"/join"} exact component={Join}/>
-        <Route path={"/user/find"} exact component={FindPassword}/>
-        <Route path={"/logout"} exact component={Logout}/>
-        <Route path={"/user/mypage"} component={MyPage}/>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path={"/"} exact component={HomeMain}/>
+          <Route path={"/login"} exact component={Login}/>
+          <Route path={"/join"} exact component={Join}/>
+          <Route path={"/logout"} exact component={Logout}/>
+          <Route path={"/user/find"} exact component={FindPassword}/>
+          <Route path={"/user/mypage"} component={MyPage}/>
+          <ItemRoutes/>
+          {isAdmin ? <AdminRoutes/> : null}
+        </Switch>
+      </BrowserRouter>
+    </>
 
-        <Route path={"/admin/home/items/carousel"} exact component={HomeItemCarousel}/>
-        <Route path={"/admin/home/items/carousel/add"} exact component={HomeItemCarouselAdd}/>
-        <Route path={"/admin/home/items/carousel/:carouselId"} exact component={HomeItemCarouselDetail}/>
-
-
-        <Route path={"/admin"} exact component={AdminMain}/>
-        <Route path={"/admin/categories"} exact component={CategoryManage}/>
-        <Route path={"/admin/subcategories"} exact component={SubcategoryManage}/>
-
-        <Route path={"/admin/items"} exact component={ItemManage}/>
-        <Route path={"/admin/items/add"} exact component={ItemAdd}/>
-        <Route path={"/admin/items/:itemId"} exact component={ItemDetail}/>
-        <Route path={"/admin/items/edit/:itemId"} exact component={ItemEdit}/>
-
-        <Route path={"/item_list"} exact component={ItemList}/>
-        <Route path={"/item/detail/:itemId"} exact component={ItemCustomerDetail}/>
-
-        <Route path={"/items"} exact component={Items}/>
-        <Route path={"/items/on_discount"} exact component={OnDiscountItemList}/>
-      </Switch>
-    </BrowserRouter>
   );
 }
 
---
+++ src/routes/admin/AdminRoutes.jsx
@@ -0,0 +1,30 @@
+import {Redirect, Route} from "react-router-dom";
+import HomeItemCarousel from "../../admin/item/home/carousel/HomeItemCarousel";
+import HomeItemCarouselAdd from "../../admin/item/home/carousel/HomeItemCarouselAdd";
+import HomeItemCarouselDetail from "../../admin/item/home/carousel/HomeItemCarouselDetail";
+import AdminMain from "../../admin/AdminMain";
+import CategoryManage from "../../admin/category/CategoryManage";
+import SubcategoryManage from "../../admin/category/SubcategoryManage";
+import ItemManage from "../../admin/item/ItemManage";
+import ItemAdd from "../../admin/item/ItemAdd";
+import ItemDetail from "../../admin/item/ItemDetail";
+import ItemEdit from "../../admin/item/ItemEdit";
+
+const AdminRoutes = (props) => {
+
+  return (<>
+    <Route path={"/admin/home/items/carousel"} exact component={HomeItemCarousel}/>
+    <Route path={"/admin/home/items/carousel/add"} exact component={HomeItemCarouselAdd}/>
+    <Route path={"/admin/home/items/carousel/:carouselId"} exact component={HomeItemCarouselDetail}/>
+    <Route path={"/admin"} exact component={AdminMain}/>
+    <Route path={"/admin/categories"} exact component={CategoryManage}/>
+    <Route path={"/admin/subcategories"} exact component={SubcategoryManage}/>
+    <Route path={"/admin/items"} exact component={ItemManage}/>
+    <Route path={"/admin/items/add"} exact component={ItemAdd}/>
+    <Route path={"/admin/items/:itemId"} exact component={ItemDetail}/>
+    <Route path={"/admin/items/edit/:itemId"} exact component={ItemEdit}/>
+  </>);
+
+}
+
+export default AdminRoutes;
